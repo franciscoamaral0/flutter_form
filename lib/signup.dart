f7ae84cb1f2601b6374e9f08915830f4069e350c
Francisco Amaral <francisco.amaral@arquiconsult.com>
2022-07-13 11:44:56 +0100

    add validation on username field

--- lib/signup.dart
+++ lib/signup.dart
@@ -84,128 +84,138 @@ class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return GestureDetector(
-      onTap: FocusScope.of(context).unfocus,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text(Strings.appName),
-          actions: [
-            IconButton(
-              onPressed: widget.onThemeModePressed,
-              icon: Icon(
-                theme.brightness == Brightness.light
-                    ? Icons.dark_mode
-                    : Icons.light_mode,
-              ),
-            ),
-          ],
-        ),
-        body: ListView(
-          padding: const EdgeInsets.all(12.0),
-          children: [
-            buildHeader(Strings.accessData),
-            TextField(
-              autofocus: true,
-              decoration: buildInputDecoration(Strings.userName),
-              textInputAction: TextInputAction.next,
-            ),
-            const SizedBox(height: 10),
-            TextField(
-              decoration: buildInputDecoration(Strings.email),
-              textInputAction: TextInputAction.next,
-              keyboardType: TextInputType.emailAddress,
-            ),
-            const SizedBox(height: 10),
-            TextField(
-              obscureText: obscureText,
-              decoration: buildInputDecoration(Strings.password).copyWith(
-                suffixIcon: ExcludeFocus(
-                  child: IconButton(
-                      onPressed: togleObscureIcon,
-                      icon: Icon(obscureText
-                          ? Icons.visibility
-                          : Icons.visibility_off)),
+    return Form(
+      child: GestureDetector(
+        onTap: FocusScope.of(context).unfocus,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text(Strings.appName),
+            actions: [
+              IconButton(
+                onPressed: widget.onThemeModePressed,
+                icon: Icon(
+                  theme.brightness == Brightness.light
+                      ? Icons.dark_mode
+                      : Icons.light_mode,
                 ),
               ),
-              textInputAction: TextInputAction.next,
-            ),
-            const SizedBox(height: 18),
-            buildHeader(Strings.personalInformation),
-            TextField(
-              decoration: buildInputDecoration(Strings.fullName),
-              textInputAction: TextInputAction.next,
-            ),
-            const SizedBox(height: 10),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  flex: 5,
-                  child: Focus(
-                    focusNode: birthDateFocusNode,
-                    descendantsAreFocusable: false,
-                    onFocusChange: (hasFocus) {
-                      if (hasFocus) {
-                        showBirthDatePicker();
-                      }
-                    },
-                    child: TextField(
-                      controller: birthDateController,
-                      readOnly: true,
-                      decoration: buildInputDecoration(Strings.birthDate),
-                      textInputAction: TextInputAction.next,
-                      keyboardType: TextInputType.number,
-                      onTap: showBirthDatePicker,
+            ],
+          ),
+          body: ListView(
+            padding: const EdgeInsets.all(12.0),
+            children: [
+              buildHeader(Strings.accessData),
+              TextFormField(
+                autofocus: true,
+                decoration: buildInputDecoration(Strings.userName),
+                textInputAction: TextInputAction.next,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: emptyValidator,
+              ),
+              const SizedBox(height: 10),
+              TextField(
+                decoration: buildInputDecoration(Strings.email),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.emailAddress,
+              ),
+              const SizedBox(height: 10),
+              TextField(
+                obscureText: obscureText,
+                decoration: buildInputDecoration(Strings.password).copyWith(
+                  suffixIcon: ExcludeFocus(
+                    child: IconButton(
+                        onPressed: togleObscureIcon,
+                        icon: Icon(obscureText
+                            ? Icons.visibility
+                            : Icons.visibility_off)),
+                  ),
+                ),
+                textInputAction: TextInputAction.next,
+              ),
+              const SizedBox(height: 18),
+              buildHeader(Strings.personalInformation),
+              TextField(
+                decoration: buildInputDecoration(Strings.fullName),
+                textInputAction: TextInputAction.next,
+              ),
+              const SizedBox(height: 10),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    flex: 5,
+                    child: Focus(
+                      focusNode: birthDateFocusNode,
+                      descendantsAreFocusable: false,
+                      onFocusChange: (hasFocus) {
+                        if (hasFocus) {
+                          showBirthDatePicker();
+                        }
+                      },
+                      child: TextField(
+                        controller: birthDateController,
+                        readOnly: true,
+                        decoration: buildInputDecoration(Strings.birthDate),
+                        textInputAction: TextInputAction.next,
+                        keyboardType: TextInputType.number,
+                        onTap: showBirthDatePicker,
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(width: 10),
-                Expanded(
-                  flex: 5,
-                  child: TextField(
-                    focusNode: phoneFocusNode,
-                    decoration: buildInputDecoration(Strings.phone),
-                    textInputAction: TextInputAction.next,
-                    keyboardType: TextInputType.phone,
+                  const SizedBox(width: 10),
+                  Expanded(
+                    flex: 5,
+                    child: TextField(
+                      focusNode: phoneFocusNode,
+                      decoration: buildInputDecoration(Strings.phone),
+                      textInputAction: TextInputAction.next,
+                      keyboardType: TextInputType.phone,
+                    ),
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 18),
-            buildHeader(Strings.contactMessage),
-            ContactTile(
-                value: emailChecked,
-                onChanged: (value) => setState(() {
-                      emailChecked = value!;
-                    }),
-                contactIcon: Icons.email,
-                contactTile: Strings.email),
-            ContactTile(
-                value: phoneCheked,
-                onChanged: (value) => setState(() {
-                      phoneCheked = value!;
-                    }),
-                contactIcon: Icons.phone,
-                contactTile: Strings.phone),
-            SwitchListTile(
-                focusNode: termsFocusNode,
-                contentPadding: const EdgeInsets.only(right: 8.0),
-                title: Text(
-                  Strings.termsMessage,
-                  style: theme.textTheme.subtitle2,
-                ),
-                value: acceptedTerms,
-                onChanged: (value) => setState(() {
-                      acceptedTerms = value;
-                    })),
-            ElevatedButton(
-              onPressed: showSignUpDialog,
-              child: const Text(Strings.signUp),
-            )
-          ],
+                ],
+              ),
+              const SizedBox(height: 18),
+              buildHeader(Strings.contactMessage),
+              ContactTile(
+                  value: emailChecked,
+                  onChanged: (value) => setState(() {
+                        emailChecked = value!;
+                      }),
+                  contactIcon: Icons.email,
+                  contactTile: Strings.email),
+              ContactTile(
+                  value: phoneCheked,
+                  onChanged: (value) => setState(() {
+                        phoneCheked = value!;
+                      }),
+                  contactIcon: Icons.phone,
+                  contactTile: Strings.phone),
+              SwitchListTile(
+                  focusNode: termsFocusNode,
+                  contentPadding: const EdgeInsets.only(right: 8.0),
+                  title: Text(
+                    Strings.termsMessage,
+                    style: theme.textTheme.subtitle2,
+                  ),
+                  value: acceptedTerms,
+                  onChanged: (value) => setState(() {
+                        acceptedTerms = value;
+                      })),
+              ElevatedButton(
+                onPressed: showSignUpDialog,
+                child: const Text(Strings.signUp),
+              )
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  String? emptyValidator(String? text) {
+    if (text == null || text.isEmpty) {
+      return Strings.errorMessageEmptyField;
+    }
   }
 
   @override
